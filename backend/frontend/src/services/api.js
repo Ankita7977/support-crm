dfff45cc6fb3a654b2177c4b5c86af347caca2d0
Ankita7977 <ankitaprajapati959457@gmail.com>
2026-06-02 12:58:39 +0530

    Update api.js

--- backend/frontend/src/services/api.js
+++ backend/frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    "https://support-crm-production-323c.up.railway.app",
+  baseURL: "https://support-crm-chsp.onrender.com"
 });
 
 export default API;
